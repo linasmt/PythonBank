3dfd0cb1cc61cae2ea6d8873710b531ea4cbddcf
linasmt <linasmati019@gmail.com>
2024-03-06 15:53:46 +0100

    user class verifications

--- User.py
+++ User.py
@@ -1,5 +1,15 @@
 class User:
-    def __init__(self, username, numero_compte, first_depot=0):
-        self.username = username
-        self.numero_compte = numero_compte
-        self.somme = first_depot
+    def __init__(self, username, account_number, first_depot=0):
+        self.username = self._validate_username(username)
+        self.account_number = self._validate_account_number(account_number)
+        self.sum = first_depot
+
+    def _validate_username(self, username):
+        if not username:
+            raise ValueError("Username cannot be empty")
+        return username
+
+    def _validate_account_number(self, account_number):
+        if len(account_number) != 13:
+            raise ValueError("Account number must be 10 number")
+        return account_number
